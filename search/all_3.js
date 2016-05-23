d5ed6fbd3b38cd365bd9e7af0c1ed3cef0e8bea7
clynamen <clynamen@gmail.com>
2016-05-24 00:11:06 +0200

    update docs, add main page

--- search/all_3.js
+++ search/all_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['setmode',['SetMode',['../class_m_x106.html#a7de70ad947eebbe398e4fcf18222a8de',1,'MX106']]],
-  ['setspeed',['SetSpeed',['../class_m_x106.html#ab347df7137e25c8a31d7c6b0c412ed76',1,'MX106']]]
+  ['nucleo_2ddynamixel',['nucleo-dynamixel',['../index.html',1,'']]]
 ];
